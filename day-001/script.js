78d377c5f6e25a7c2f61409e246fb5852a35afb0
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2023-10-12 20:35:38 +0530

    Update script.js

--- day-001/script.js
+++ day-001/script.js
@@ -1,3 +1,21 @@
-alert("hello world");
+const myPromise = new Promise((resolve, reject) => {
+  // Simulate an asynchronous operation (e.g., waiting for 2 seconds)
+  setTimeout(() => {
+    const randomValue = Math.random();
+    
+    if (randomValue < 0.5) {
+      resolve("Operation successful: " + randomValue);
+    } else {
+      reject("Operation failed: " + randomValue);
+    }
+  }, 2000);
+});
 
-console.log("hello server");
+// To handle the Promise's resolution or rejection, use the .then() and .catch() methods:
+myPromise
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.error(error);
+  });
