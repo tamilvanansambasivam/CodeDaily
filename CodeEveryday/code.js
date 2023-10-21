b85495bade2220460228856a10040319877a0b59
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2023-10-21 22:56:25 +0530

    Update code.js

--- CodeEveryday/code.js
+++ CodeEveryday/code.js
@@ -1,13 +1,1 @@
-const printFirst = () => console.log("First");
-const printSecondWithDelay = () => setTimeout(() => console.log("Second"), 500);
-const printThird = () => console.log("Third");
-
-printFirst();
-printSecondWithDelay();
-printThird();
-
-
-/*
-"First" and "Third" are logged immediately, and "Second" is logged 
-after a 500 ms delay due to the setTimeout function in printSecondWithDelay.
-*/
+console.log("Keep coding; victory awaits in the stack.")
