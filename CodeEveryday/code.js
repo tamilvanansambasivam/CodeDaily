ec5a0dd8d3afc014748686ca4b5981760dbce74c
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2023-12-06 08:50:11 +0530

    Update code.js

--- CodeEveryday/code.js
+++ CodeEveryday/code.js
@@ -14,3 +14,9 @@ let closure = outerFunction();
 // Executing the closure
 closure(); // Outputs: I am from the outer function
 
+// A closure is the combination of a function bundled together (enclosed) 
+// with references to its surrounding state (the lexical environment). 
+// In other words, a closure gives you access to an outer function's scope 
+// from an inner function. In JavaScript, closures are created every time 
+// a function is created, at function creation time.
+
