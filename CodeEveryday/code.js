e6f0093bf4293f00c794d9030ed48f3c1046d1bd
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2023-12-06 08:49:24 +0530

    closures

--- CodeEveryday/code.js
+++ CodeEveryday/code.js
@@ -1,1 +1,16 @@
-console.log("Older problems often have timeless solutions")
+function outerFunction() {
+  let outerVariable = 'I am from the outer function';
+
+  function innerFunction() {
+    console.log(outerVariable);
+  }
+
+  return innerFunction;
+}
+
+// Creating a closure by assigning the inner function to a variable
+let closure = outerFunction();
+
+// Executing the closure
+closure(); // Outputs: I am from the outer function
+
