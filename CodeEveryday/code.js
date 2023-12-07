0961c4e3263038dbfceb6b9e592135fb65f975b7
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2023-12-07 23:48:59 +0530

    Update code.js

--- CodeEveryday/code.js
+++ CodeEveryday/code.js
@@ -1,22 +1,12 @@
-function outerFunction() {
-  let outerVariable = 'I am from the outer function';
-
-  function innerFunction() {
-    console.log(outerVariable);
-  }
-
-  return innerFunction;
+// Function Declaration
+console.log(multiply(2, 3)); // Hoisting allows calling before the declaration
+function multiply(x, y) {
+  return x * y;
 }
 
-// Creating a closure by assigning the inner function to a variable
-let closure = outerFunction();
-
-// Executing the closure
-closure(); // Outputs: I am from the outer function
-
-// A closure is the combination of a function bundled together (enclosed) 
-// with references to its surrounding state (the lexical environment). 
-// In other words, a closure gives you access to an outer function's scope 
-// from an inner function. In JavaScript, closures are created every time 
-// a function is created, at function creation time.
-
+// Function Expression
+// console.log(divide(6, 2)); // This would result in an error because the function expression is not hoisted
+var divide = function(x, y) {
+  return x / y;
+};
+console.log(divide(6, 2)); // This works fine when the function expression is defined before calling
